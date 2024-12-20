3647d93b1d9c5c6e306508db8791c0dd48203f3e
Gray Matter <graymatter.space@gmail.com>
2024-12-20 17:39:50 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,12 +2,16 @@
 
 function core(){
 	echo -e "\e[32mInstalling Core System utils\e[0m"
-	sudo pacman -Syu alsa-firmware alsa-plugins alsa-utils gst-libav gst-plugin-pipewire gst-plugins-bad gst-plugins-ugly libdvdcss pavucontrol pipewire-alsa pipewire-jack pipewire-pulse rtkit sof-firmware wireplumber mesa mesa-utils nvidia nvidia-utils xf86-input-libinput xf86-video-amdgpu xf86-video-ati xorg-server xorg-xdpyinfo xorg-xinit xorg-xinput xorg-xkill xorg-xrandr intel-ucode firefox neovim btop htop neofetch telegram-desktop wget python-pywal ranger rofi kitty python-pillow git tor flameshot npm vlc pavucontrol discord kleopatra keepassxc pidgin qbittorrent thunar picom
+	sudo pacman -Syu alsa-firmware alsa-plugins alsa-utils gst-libav gst-plugin-pipewire gst-plugins-bad gst-plugins-ugly libdvdcss pavucontrol pipewire-alsa pipewire-jack pipewire-pulse rtkit sof-firmware wireplumber mesa mesa-utils nvidia nvidia-utils xf86-input-libinput xf86-video-amdgpu xf86-video-ati xorg-server xorg-xdpyinfo xorg-xinit xorg-xinput xorg-xkill xorg-xrandr intel-ucode firefox neovim btop htop neofetch telegram-desktop wget python-pywal ranger rofi kitty python-pillow git tor flameshot npm vlc pavucontrol discord kleopatra keepassxc pidgin qbittorrent thunar picom i3 zsh feh xclip
 }
 
 function demon_settings(){
 	echo -e "\e[32mSetting up Demons\e[0m"
 	sudo systemctl enable tor
+}
+
+function custom_picom(){
+	yay -S picom-simpleanims-next-git
 }
 
 function configure_aur_yay(){
@@ -19,8 +23,7 @@ function configure_aur_yay(){
 
 function setup_config(){
 	echo -e "\e[32mSetting up configs\e[0m"
-	git cloning the repo 
-	git clone https://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@github.com/graymatter10/dotfilesv2 /tmp/dotfiles
+	git clone https://github.com/graymatter10/dotfilesv2 /tmp/dotfiles
 	cd /tmp/dotfiles/config
 	cp -r kitty nvim rofi wal picom ranger tgpt tmux ~/.config/
 }
@@ -39,6 +42,7 @@ function wm_config(){
 	if [ $wm = "i3" ]; then 
 		echo -e "\e[32mSetting up i3\e[0m"
 		cd /tmp/dotfiles/config
+		echo "copying i3"
 		cp -r i3 rofi ~/.config/
 	elif [ $wm = "dwm" ]; then 
 		echo -e "\e[31mdwm not Available\e[0m"
@@ -51,7 +55,7 @@ function wm_config(){
 function download_wallpapers(){
 	echo -e "\e[32mSetting up Wallpapers\e[0m"
 	mkdir ~/Pictures
-	git clone https://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@github.com/graymatter10/wallpapers ~/Pictures/Wallpapers/
+	git clone https://github.com/graymatter10/wallpapers ~/Pictures/Wallpapers/
 }
 
 echo -n "Available Gui Option
@@ -76,19 +80,20 @@ if [[ $cwal != "y" && $cwal != "n" ]]; then
 fi
 
 echo -e "\e[32mPlease Ctrl+c if you wanna don't want the configs\e[0m"
-sleep 5
 
 echo "
 
 "
 
-core
+#core
 demon_settings
 setup_config
 wm_config $wm
-zsh_config
+configure_aur_yay
+custom_picom
 if [ $cwal = "y" ]; then 
 	download_wallpapers
 fi
+zsh_config
 
 
